4ac42858030751bbc55dfb810da2cadc1b760f0d
Noob-Bob <105913432+Noob-Bob@users.noreply.github.com>
2023-03-02 12:45:53 -0800

    added rest controller and modified port

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -3,11 +3,20 @@ package com.kob.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
 @RestController
 @RequestMapping("/pk/")
 public class BotInfoController {
   @RequestMapping("getbotinfo/")
-  public String getBotInfo() {
-    return "114514";
+  public Map<String, Integer> getBotInfo() {
+    Map<String, Integer> map = new HashMap<>();
+    map.put("sword", 3800);
+    map.put("axe", 10);
+    map.put("shield", 15);
+    return map;
   }
 }
