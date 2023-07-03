f4f1134f5cfcc802e02a8e36f00b88a1b37dd5ee
Noob-Bob <105913432+Noob-Bob@users.noreply.github.com>
2023-07-03 16:08:03 +0800

    Implemented static frontend for user matching

--- web/src/views/pk/PkindexView.vue
+++ web/src/views/pk/PkindexView.vue
@@ -1,14 +1,17 @@
 <template>
-        <PlayGround />        
+        <PlayGround v-if="$store.state.pk.status === 'playing'"/>
+        <MatchGround v-if="$store.state.pk.status === 'matching'"/>
 </template>
 
 <script>
 import PlayGround from '../../components/PlayGround.vue'
+import MatchGround from '../../components/MatchGround.vue'
 import { onMounted, onUnmounted } from 'vue';
 import { useStore } from 'vuex';
 export default {
     components: {
-        PlayGround
+        PlayGround,
+        MatchGround,
     },
     setup() {
         const store = useStore();
@@ -16,6 +19,10 @@ export default {
 
         let socket = null;
         onMounted(() => {
+            store.commit("updateOpponent", {
+                username: "My opponent",
+                photo: "https://cdn.acwing.com/media/article/image/2022/08/09/1_1db2488f17-anonymous.png",
+            })
             socket = new WebSocket(socketUrl);
 
             socket.onopen = () => {
