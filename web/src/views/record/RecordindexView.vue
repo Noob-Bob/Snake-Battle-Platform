bff302ff218bbb9772eae5f0e6f7fde78164cc57
Noob-Bob <105913432+Noob-Bob@users.noreply.github.com>
2023-07-31 18:28:24 +0800

    Battle record pagination

--- web/src/views/record/RecordindexView.vue
+++ web/src/views/record/RecordindexView.vue
@@ -28,6 +28,19 @@
                 </tr>
             </tbody>
         </table>
+        <nav aria-label="...">
+            <ul class="pagination" style="float: right;">
+              <li class="page-item" @click="click_page(-2)">
+                <a class="page-link" href="#">Previous</a>
+              </li>
+              <li :class="'page-item ' + page.is_active" v-for="page in pages" :key="page.number" @click="click_page(page.number)">
+                <a class="page-link" href="#">{{ page.number }}</a>
+              </li>
+              <li class="page-item" @click="click_page(-1)">
+                <a class="page-link" href="#">Next</a>
+              </li>
+            </ul>
+          </nav>
     </ContentField>         
 </template>
 
@@ -47,6 +60,31 @@ export default {
         let records = ref([]);
         let current_page = 1;
         let total_records = 0;
+        let pages = ref([]);
+
+        const click_page = page => {
+            if (page === -2) page = current_page - 1;
+            else if (page === -1) page = current_page + 1; 
+            let max_pages = parseInt(Math.ceil(total_records / 10));
+
+            if (page >= 1 && page <= max_pages) {
+                pull_page(page);
+            }
+        }
+
+        const update_pages = () => {
+            let max_pages = parseInt(Math.ceil(total_records / 10));
+            let new_pages = [];
+            for (let i = current_page - 2; i <= current_page + 2; i ++) {
+                if (i >= 1 && i <= max_pages) {
+                    new_pages.push({
+                        number: i,
+                        is_active: i === current_page ? "active" : "",
+                    });
+                }
+            }
+            pages.value = new_pages;
+        }
 
         console.log(total_records);
 
@@ -64,6 +102,7 @@ export default {
                 success(resp) {
                     records.value = resp.records;
                     total_records = resp.records_count;
+                    update_pages();
                 },
                 error(resp) {
                     console.log(resp);
@@ -117,7 +156,9 @@ export default {
 
         return {
             records,
-            open_record_content
+            open_record_content,
+            pages,
+            click_page
         }
     }
 
